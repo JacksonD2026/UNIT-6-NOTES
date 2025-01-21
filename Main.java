3c6ad24054120288e0cb7c41c1737ad3dcb9b0ab
jackson <jackson.dobson2026@gbwl.org>
2025-01-21 13:49:54 +0000

    favorite foods

--- Main.java
+++ Main.java
@@ -21,6 +21,25 @@ public class Main {
       // Need to tell java how many spaces to allocate
       luckyNumbers = new int[6]; // already delcared earlier
       int[] moreLuckyNumbers = new int[8]; //declare + create in one line
+      double[] movieRatings = new double[10];
+      String[] faveAnimals = new String[8];
+      System.out.println(moreLuckyNumbers);
+
+      // INITIALIZER LIST
+      int[] luckiestNumbers = {13,13,10,9,5,20,37,7};
+      double[] currentCash = {1.0 , 5.25, 15.0, 21.0};
+      String[] bestClass = { "Maia", "Alex","Zoie", "Paige", "Natalie", "Bryce", "Finny", "Jackson"};
+      
+      //How to access array elements
+      // arrayname[index] --> gets item at that position
+      System.out.println(luckiestNumbers[5]);
+      System.out.println(bestClass[5]);
+
+      // HOW TO MODIFY AN ARRAY ELEMENT
+      // arrayName[index] = newValue
+      faveAnimals[0] = "dolphin";
+      faveAnimals[1] = "dog";
+
 
 
 
